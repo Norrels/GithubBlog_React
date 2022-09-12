51cb8e7a983b2fa3ac02202a63bbeae9de362579
Matheus <Matheuus412@gmail.com>
2022-09-12 16:18:49 -0300

    :tada: Commit inicial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import background from '../../assets/background.svg'
+
+export function Home (){
+    return (
+        <>
+        <img src={background} alt="" />
+         <h1>Hello world</h1>
+        </>
+       
+    )
+}
